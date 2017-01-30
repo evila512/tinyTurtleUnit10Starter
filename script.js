a3806381f472db941caa1c905ab1527a214c5999
Evila Santos <evilavsantos@gmail.com>
2017-01-30 23:45:07 +0000

    Tiny Turtle change

--- script.js
+++ script.js
@@ -12,7 +12,37 @@ right(90);
 forward(50);
 }
 
-// Type your function call below
+function triangle(){
 
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(150);
+forward(80);
+}
+
+function house(){
+    
+square();
+triangle();
+}
+
+house();
+
+function pentagon(){
+forward(10);
+right(30);
+forward(40);
+right(120);
+forward(-30);
+right(119);
+forward(15);
+right(96);
+forward(-80);
+}
+pentagon();
 
 stamp();
+
+
